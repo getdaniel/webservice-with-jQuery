7faf6948123530b76aaeb8127c9e0e6dcd8d1651
Me <getdaniel>
2023-02-09 17:26:57 +0300

    refactored file

---
+++ src/main/java/com/ws/webservicewithjQuery/WebserviceWithJQueryApplication.java
@@ -0,0 +1,13 @@
+package com.ws.webservicewithjQuery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceWithJQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceWithJQueryApplication.class, args);
+	}
+
+}

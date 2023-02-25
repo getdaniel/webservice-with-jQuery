6a4572d9e725b106b5e6bf1bc1caf15df199d477
Minuyelet <80815559+Minuyelet@users.noreply.github.com>
2023-02-25 15:10:46 +0300

    Update User.java

--- src/main/java/com/ws/webservicewithjQuery/User.java
+++ src/main/java/com/ws/webservicewithjQuery/User.java
@@ -8,7 +8,7 @@ class User {
 	  public User() {}
 	  
 	  public User(int id, String name, String email) {
-		this.id = id;
+	    this.id = id;
 	    this.name = name;
 	    this.email = email;
 	  }
